de6d3ae7e66a2f26da0948902762cf22e487ae05
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2021-09-19 02:47:10 +0530

    Delete components directory

--- components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as ChannelContainer } from './ChannelContainer';
-export { default as ChannelListContainer } from './ChannelListContainer';
-export { default as ChannelSearch } from './ChannelSearch';
