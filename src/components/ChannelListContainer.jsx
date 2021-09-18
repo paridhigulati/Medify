61d2d6d854c7aaf7611ebb1554731321e280aee6
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2021-09-19 01:59:41 +0530

    initial commit

---
+++ src/components/ChannelListContainer.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { ChannelList, useChatContext } from 'stream-chat-react';
+//import Cookies from 'universal-Cookie';
+import {ChannelSearch, TeamChannelList, TeamChannelPreview} from './';
+import HospitalIcon from '../assets/hospital.png'
+import LogoutIcon from '../assets/logout.png'
+const SideBar = () => (
+    <div className = "channel-list__sidebar">
+<div className = "channel-list__sidebar__icon1">
+    <div className = "icon1__inner">
+        <img src = {HospitalIcon}  alt = "Hospital" width = "30" />
+    </div>
+    </div>
+    <div className = "channel-list__sidebar__icon2">
+    <div className = "icon1__inner">
+        <img src = {LogoutIcon}  alt = "Logout" width = "30" />
+    </div>
+    </div>
+</div>
+);
+const CompanyHeader = () => (
+    <div className = "channel-List___header">
+        <p className = "channel-list__header__text">Medify</p>
+    </div>
+)
+
+const ChannelListContainer = () => {
+    return (
+        <>
+            <SideBar />
+            <div className = "channel-list__list__wrapper">
+                <CompanyHeader />
+            </div>
+        </>
+    );
+}
+
+export default ChannelListContainer;
