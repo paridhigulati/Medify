7d477c0d5bd3334145a6b07a4b3a2cd03bc55ee9
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2021-09-21 02:21:23 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,5 @@ export { default as ChannelContainer } from './ChannelContainer';
 export { default as ChannelListContainer } from './ChannelListContainer';
 export { default as ChannelSearch } from './ChannelSearch';
 export { default as TeamChannelist} from './TeamChannelList';
+export { default as TeamChannelPreview} from './TeamChannePreview';
+export { default as Auth } from './Auth';
