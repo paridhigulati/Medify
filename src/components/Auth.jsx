64b546876bd4a190c06c6bd307c4bcc45d80df88
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2021-09-22 02:44:47 +0530

    Update Auth.jsx

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,17 +1,64 @@
 import React, { useState } from 'react';
-import Cookies from 'universal-cookies';
+import Cookies from 'universal-cookie';
 import axios from 'axios';
+
 import signinImage from '../assets/signup.jpg';
 
+const cookies = new Cookies();
+
+const initialState = {
+    fullName: '',
+    username: '',
+    password: '',
+    confirmPassword: '',
+    phoneNumber: '',
+    avatarURL: '',
+}
+
 const Auth = () => {
-    const [isSignup, setIsSignup] = useState(false);
-   const handleChange = () => {}
+    const [form, setForm] = useState(initialState);
+    const [isSignup, setIsSignup] = useState(true);
+
+    const handleChange = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value });
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const { username, password, phoneNumber, avatarURL } = form;
+
+        const URL = 'https://localhost:5000/auth';
+        // const URL = 'https://medical-pager.herokuapp.com/auth';
+
+        const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
+            username, password, fullName: form.fullName, phoneNumber, avatarURL,
+        });
+
+        cookies.set('token', token);
+        cookies.set('username', username);
+        cookies.set('fullName', fullName);
+        cookies.set('userId', userId);
+
+        if(isSignup) {
+            cookies.set('phoneNumber', phoneNumber);
+            cookies.set('avatarURL', avatarURL);
+            cookies.set('hashedPassword', hashedPassword);
+        }
+
+        window.location.reload();
+    }
+
+    const switchMode = () => {
+        setIsSignup((prevIsSignup) => !prevIsSignup);
+    }
+
     return (
-        <div className = "auth__form-container">
-            <div className = "auth__form-container__fields">
-                <div className = "auth__form-container__fields-content">
-                    <p> {isSignup?'Sign Up': 'Sign In' } </p>
-                    <form onSubmit={() => {}}>
+        <div className="auth__form-container">
+            <div className="auth__form-container_fields">
+                <div className="auth__form-container_fields-content">
+                    <p>{isSignup ? 'Sign Up' : 'Sign In'}</p>
+                    <form onSubmit={handleSubmit}>
                         {isSignup && (
                             <div className="auth__form-container_fields-content_input">
                                 <label htmlFor="fullName">Full Name</label>
@@ -24,7 +71,7 @@ const Auth = () => {
                                 />
                             </div>
                         )}
-                    <div className="auth__form-container_fields-content_input">
+                        <div className="auth__form-container_fields-content_input">
                             <label htmlFor="username">Username</label>
                                 <input 
                                     name="username" 
@@ -34,7 +81,7 @@ const Auth = () => {
                                     required
                                 />
                             </div>
-                            {isSignup && (
+                        {isSignup && (
                             <div className="auth__form-container_fields-content_input">
                                 <label htmlFor="phoneNumber">Phone Number</label>
                                 <input 
@@ -46,7 +93,7 @@ const Auth = () => {
                                 />
                             </div>
                         )}
-                    {isSignup && (
+                        {isSignup && (
                             <div className="auth__form-container_fields-content_input">
                                 <label htmlFor="avatarURL">Avatar URL</label>
                                 <input 
