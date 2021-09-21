bdcaf737e08fa69e84cd66434bbda46200c3bc59
Paridhi Gulati <54714471+paridhigulati@users.noreply.github.com>
2021-09-22 01:49:26 +0530

    updated index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+
+const authRoutes = require("./routes/auth.js");
 const app = express();
 const PORT = process.env.PORT || 5000;
 require('dotenv').config();
@@ -7,3 +9,8 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
+app.get('/', (req, res)=> {
+    res.send('Hello word');
+})
+app.use('./auth', authRoutes);
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
